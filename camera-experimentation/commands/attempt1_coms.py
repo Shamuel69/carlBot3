6bba1dbf05dce46200671663c8dcaa6c12df118b
Samuel the Ultimate <tobbanganmantis@gmail.com>
2025-05-27 03:08:52 -0400

    colorsliders

--- camera-experimentation/commands/attempt1_coms.py
+++ camera-experimentation/commands/attempt1_coms.py
@@ -6,9 +6,9 @@ from PIL import Image, ImageTk
 
 ## functions for menus in attempt1.py
 
-class App_func:
-    def __init__(self):
-        self.Rnum = 
+# class App_func:
+#     def __init__(self):
+#         self.Rnum = 
 
 
 
@@ -30,23 +30,46 @@ class App_func:
 #                 r, g, b = colorsys.hsv_to_rgb(hue, saturation, value)
 #                 self.create_rectangle(x, y, x + 1, y + 1, fill="#%02x%02x%02x" % (int(r * 255), int(g * 255), int(b * 255)))
 class colorwheel:
-    def __init__(self, width=300, height=200):
+    def __init__(self, width=500, height=200):
         self.width = width
         self.height = height
         
         self.root = tk.Tk()
+
+        self.left_panel = tk.Frame(self.root, width=width, height=height)
+        self.left_panel.pack(side="left")
+
+        self.right_panel = tk.Frame(self.root, width=width, height=height, background="#3a2865")
+        self.right_panel.pack(side="right")
+
         self.img_data = self.generate_gradient_image(width, height)
         self.photo_image = ImageTk.PhotoImage(self.img_data)
 
 
-        self.canvas = tk.Canvas(self.root, width=width, height=height)
+        self.canvas = tk.Canvas(self.left_panel, width=width, height=height)
         self.canvas.pack()
         self.canvas.create_image(0, 0, anchor="nw", image=self.photo_image)
         self.canvas.image = self.photo_image
         self.canvas.bind("<Button-1>", self.get_color)
         
-        self.label = tk.Label(self.root, text="Click a color", font=("Arial", 14), width=20)
-        self.label.pack(pady=10)
+        #side color panel
+        self.label = tk.Label(self.right_panel, text="Click a color", font=("Arial", 14), width=20)
+        self.label.grid(row= 0, column=0, pady=10)
+
+        self.label2 = tk.Label(self.right_panel, text="Color Mode", font=("Arial", 12), width=20)
+        self.label2.grid(row= 1, column=0)
+
+        #sliders
+        self.scale1 = tk.Scale(self.right_panel, from_=0, to=255, orient="horizontal", length=150, label="gog")
+        self.scale2 = tk.Scale(self.right_panel, from_=0, to=255, orient="horizontal", length=150, )
+        self.scale3 = tk.Scale(self.right_panel, from_=0, to=255, orient="horizontal", length=150, )
+        self.scale1.grid(row= 0, column=1, pady=7)
+        self.scale2.grid(row= 1, column=1, pady=7)
+        self.scale3.grid(row= 2, column=1, pady=7)
+
+        self.dropdown = tk.OptionMenu(self.right_panel, tk.StringVar(), "rgb", "hsv", "grayscale")
+        self.dropdown.grid(row= 2, column=0)
+
         
     def generate_gradient_image(self, width, height):
         img = Image.new("RGB", (width, height))
@@ -66,9 +89,15 @@ class colorwheel:
             print(f"selected color: {hex_color}")
             self.label.config(text=f"selected color: {hex_color}")
     
-    def colorlabel():
+    def colorset(self):
+        print(self.dropdown.get())
+        if self.dropdown.get() == "rgb":
+            self.label2.config(text="RGB")
+        elif self.dropdown.get() == "hsv":
+            self.label2.config(text="HSV")
+        elif self.dropdown.get() == "grayscale":
+            self.label2.config(text="Grayscale")
         #make an adjustable rgb slider that changes the color
-        pass
 
     def run(self):
         self.root.mainloop()
