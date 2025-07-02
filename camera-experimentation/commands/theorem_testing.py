48c9034b117bb2dcc22ea30a5f167169e95083c4
Samuel the Ultimate <tobbanganmantis@gmail.com>
2025-07-02 03:56:57 -0400

    wrong_equation

--- camera-experimentation/commands/theorem_testing.py
+++ camera-experimentation/commands/theorem_testing.py
@@ -1,5 +1,22 @@
 import math
 
+### indi = individual 
+
+def adjustable_matrix(height: int, rows:int):
+    mother_exponent = -11.62413
+    slope = 202.14817
+    MF_value = slope*(height**mother_exponent)
+    matrix = []
+    
+    for iter in range(height):
+        mini_matrix = []
+        for i in range(rows):
+            indi_value = rows-((slope*(height**mother_exponent))**i)
+            print((slope*(height**mother_exponent))**i)
+            mini_matrix.append(indi_value)
+        matrix.append(mini_matrix)   
+    
+    return matrix
 
 def bingus(longways:int):
     var = []
@@ -39,5 +56,9 @@ def playing_with_matrix(n:int, Width:int):
     matrix = matrix[1:]
     return matrix
 
+def print_matrix(matrix):
+    for row in matrix:
+        print(" ".join(str(x) for x in row))
+
 # bingus(100)
-print(playing_with_matrix(360, 6))
+print(adjustable_matrix(20, 5))
